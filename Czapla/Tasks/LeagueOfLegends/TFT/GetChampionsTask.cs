74dd3e74435d611ad8a7b1af62c4f00d7f13d379
capthapl <dawidkacprzak@icloud.com>
2019-07-24 21:08:48 +0200

    Created project 'Drzewo' for MongoDB things

---
+++ Czapla/Tasks/LeagueOfLegends/TFT/GetChampionsTask.cs
@@ -0,0 +1,16 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Czapla.Tasks.LeagueOfLegends.TFT
+{
+    public class GetChampionsTask : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Drzewo/MongoController.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Drzewo
+{
+    public class Class1
+    {
+    }
+}
