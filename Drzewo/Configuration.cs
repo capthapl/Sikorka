21a23d50856822829ff7fdf51a8547830026a343
capthapl <dawidkacprzak@icloud.com>
2019-08-13 07:09:42 +0200

    Added placeholder function and comment purpose

--- Drzewo/Configuration.cs
+++ Drzewo/Configuration.cs
@@ -7,5 +7,12 @@ namespace Drzewo
     public static class Configuration
     {
         public const string MongoConnectionString = "mongodb://rekurencja:hermetyzacj4!@localhost:27017";
+
+        public static void FetchConfiguration()
+        {
+            //here should be login for download configuration from mongodb.
+            //before run any program/service config should be fetched and if failed then terminate.
+            //This feature allow us to get configuration without recompile projects.
+        }
     }
 }
